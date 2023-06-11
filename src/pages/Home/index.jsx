3a2105369b5f3d0ee7945535a21241cd2d92a466
lbseven7 <leobarbosapinturas@gmail.com>
2023-06-11 16:52:16 -0300

    feat:  home mobile finalizada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,8 +6,6 @@ const Home = () => {
     height: '100vh',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
     fontFamily: 'Roboto',
     color: '#fff',
     fontWeight: 'bold',
@@ -16,32 +14,38 @@ const Home = () => {
   
   return (
     <>
-    <section style={sectionHome}>
+      <section style={sectionHome}
+     >
 
-      <div style={{ position: 'relative', marginBottom: '5px', display: 'flex', flexDirection: 'column', marginTop: '200px' }}>
+        <div style={{
+          position: 'absolute',
+          marginBottom: '5px',
+          marginTop: '200px',
+
+        }}>
 
         <div style={{  }}>
-            <p style={{marginLeft: '-35px',fontSize: '0.5rem', marginTop: '170px'  }}>Olá, Eu Sou o ALEXSANDRO</p>
+            <p style={{marginLeft: '-35px',fontSize: '0.5rem', marginTop: '17px'  }}>Olá, Eu Sou o ALEXSANDRO</p>
         </div>
 
-        <div style={{position: 'absolute',top: '150px', marginLeft: '-77px' }}>
+        <div style={{position: 'absolute',top: '-5px', marginLeft: '-77px' }}>
           <h1 style={{margin: '32px', marginBottom: '0px',fontSize: '3rem', color: '#F18403'}}>FRONTEND</h1>
         </div>
 
-        <div style={{ position: 'absolute', bottom: '0', top: '220px' }}>
+        <div style={{ position: 'absolute', bottom: '0', top: '60px' }}>
             <h2 style={{marginLeft: '-45px',fontSize: '3rem'}}>DEVELOPER</h2>
         </div>
 
-        </div>
+      </div>
         
-        <div style={{ marginRight: '100px', fontSize: '1.5rem', width: '35vw', height:'20vh', marginTop: '350px', marginLeft: '650px' }}>
+        <div style={{ position: 'absolute', marginTop: '350px' }}>
            <h3>
               Tenho 40 anos, sou desenvolvedor frontend 
               <br />com foco em ReactJS e NodeJS.
               Tenho 16 meses <br />de experiência trabalhando 
               como freelancer.
             </h3>
-       </div>
+        </div>
        
             
     </section> 
