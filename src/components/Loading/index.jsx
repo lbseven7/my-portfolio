143e77f4e3829a8763ae60cc348f7c06559a0bf2
lbseven7 <leobarbosapinturas@gmail.com>
2023-06-10 21:33:47 -0300

    feat: define a imagem para o loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import loading from '../../images/desktop/loading1.png';
 
 const Loading = () => {
   return (
     <div>
-      <h2>Loading...</h2>
-      <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading" />
+      {/* <h2>Loading...</h2> */}
+      {/* <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading" /> */}
+      <img src={loading} alt="loading" />
     </div>
   );
 };
