4fde9fafb2acf9bc5abcc7e54a19b818e3526797
lbseven7 <leobarbosapinturas@gmail.com>
2023-06-10 21:23:44 -0300

    feat: cria componente de Loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div>
+      <h2>Loading...</h2>
+      <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="loading" />
+    </div>
+  );
+};
+
+export default Loading;
