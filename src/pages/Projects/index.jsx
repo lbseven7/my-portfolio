15a9ef23af35756aadeed6521e3b4a2c7d3bb95e
lbseven7 <leobarbosapinturas@gmail.com>
2023-06-15 20:36:44 -0300

    feat: adiciona projeto e links

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,5 +1,6 @@
 import work1 from '../../images/desktop/projeto_01.png';
 import work2 from '../../images/desktop/projeto_02.png';
+import work3 from '../../images/desktop/projeto.png';
 import '../Projects/projects.css';
 
 const Projects = () => {
@@ -14,7 +15,9 @@ const Projects = () => {
           <h3>ESTUDO DE CASO</h3>
           <h2>SITE DE SISTEMA DE<br /> CHAMADA ESCOLAR</h2>
 
-          <button type='button'>VER PROJETO</button>
+          <button type='button'>
+           <a href='https://chamada-escolar.vercel.app/' target='_blank' rel="noreferrer">VER PROJETO</a> 
+          </button>
         </div>
         
       </section> 
@@ -27,10 +30,28 @@ const Projects = () => {
             <h3>ESTUDO DE CASO</h3>
             <h2>SITE DIVULGAÇÃO PARA<br /> PLANTIO DE PAU BRASIL</h2>
 
-            <button type='button'>VER PROJETO</button>
+          <button type='button'>
+           <a href='https://reflorbrasil.com.br' target='_blank' rel="noreferrer">VER PROJETO</a> 
+          </button>
          </div>
        
       </section> 
+
+          
+      <section className='projects'>
+        <div className='project-two' >
+        
+            <img src={work3} alt="work3"/>
+               
+            <h3>ESTUDO DE CASO</h3>
+            <h2>SITE DE COMPRAS ONLINE<br /> SHOPPING CART</h2>
+
+          <button type='button'>
+           <a href='https://redux-shopping-cart-kappa.vercel.app/' target='_blank' rel="noreferrer">VER PROJETO</a> 
+          </button>
+         </div>
+       
+      </section>
 
     </>
   );
